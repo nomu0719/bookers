c37ff2fae69955af70a3dde4f4fbcd63941f0110
kei53415@gmail.com <ec2-user@ip-172-31-61-147.ec2.internal>
2021-03-09 10:36:31 +0000

    css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def new
@@ -9,8 +10,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to'/books'
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def show
@@ -23,8 +29,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to'/books'
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+     else
+      render()
+    end
   end
 
   def destroy
@@ -32,7 +42,7 @@ class BooksController < ApplicationController
     @book.destroy
     redirect_to'/books'
   end
-  
+
 
  private
   def book_params
